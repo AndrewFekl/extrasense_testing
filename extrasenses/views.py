4ed0521d66b17fa44ed09d291598c66ab106842e
Andrey Feklichev <feklichev@mail.ru>
2021-12-22 17:04:43 +0500

    Initial commit

---
+++ extrasenses/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+class Extrasens:
+    def __init__(self, name):
+        self.name = name
+        self.attempts = 0
+        self.coincidence = 0
+        self.raiting = 0
+
+    def guess_number(self):
+        return 5
+
+
+
+
+class ExtrasensesView(View):
+    """Класс, моделирующий тестирование экстрасенсорных способностей. При вызове метода GET отображает приглашение
+    число и нулевые рейтинги экстрасенсов. При вызове метода POST будут отображаться ответы экстрасенсов,
+    обновляться статистика и рейтинги экстрасенсов. В зависимости от установки триггера guess_trigger будет чередаватся
+    показ результатов и приглашение загадать число"""
+
+    def __init__(self, *args, **kwargs):
+        super(ExtrasensesView, self).__init__(*args, **kwargs)
+        # триггер режима страницы. Если True - требуется загадать слово. Если False - ввести ответ.
+        self.guess_trigger = True
+        # список загаданных слов пользователя
+        self.user_story = []
+        # словарь для хранения истории ответов экстрасенсов
+        self.extrasenses = {'1': Extrasens('John'), '2': Extrasens('Moana')}
+        self.extrasenses_stories = {'1': [], '2': []}
+
+
+    def get(self, request):
+        context = {'guess_trigger': self.guess_trigger, 'user_story': self.user_story, 'extrasenses': self.extrasenses}
+        self.guess_trigger = False
+        return render(request, 'extrasenses/extrasenses.html', context)
+
+    def post(self, request):
+        number = request.POST['put_up_number']
+        for key, extrasens in self.extrasenses.items():
+            answer = extrasens.guess_number()
+            extrasens.attempts += 1
+            if number == answer:
+                extrasens.coincidence += 1
+            if extrasens.attempts != 0:
+                extrasens.raiting = extrasens.coincidence / extrasens.attempts * 100
+
+            self.extrasenses_stories.update({key: self.extrasenses_stories[key].append(answer)})
+
+
+
+
+
+
+
+
+# Create your views here.
